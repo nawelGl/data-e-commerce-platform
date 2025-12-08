db4323afa4fa553d133117fc80edf3956f252aaf
nawelGl <ghazal.nawel@gmail.com>
2025-12-08 22:20:38 +0100

    Update ProductController.java

--- TP1/ms-product/src/main/java/com/episen/ms_product/infrastructure/web/controller/ProductController.java
+++ TP1/ms-product/src/main/java/com/episen/ms_product/infrastructure/web/controller/ProductController.java
@@ -1,32 +1,26 @@
 package com.episen.ms_product.infrastructure.web.controller;
 
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.episen.ms_product.application.dto.ProductRequestDTO;
-import com.episen.ms_product.application.dto.ProductResponseDTO;
-import com.episen.ms_product.application.service.ProductService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.episen.ms_product.application.dto.ProductRequestDTO;
+import com.episen.ms_product.application.dto.ProductResponseDTO;
+import com.episen.ms_product.application.service.ProductService;
+
+import java.net.URI;
+import java.util.List;
 
 /**
  * Contrôleur REST pour la gestion des produits.
